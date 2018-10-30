0bba0416fd13d59e99c0e4548620f5bd935b9c9e
icething <whittiger.98@mail.ru>
2018-10-30 13:51:03 +0300

    Выполнил HW3_easy

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -5,7 +5,10 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    round_number = 10 ** ndigits
+    number_2 = int(number * round_number + 0.5)
+    scale_number = number_2 / round_number
+    return scale_number
 
 
 print(my_round(2.1234567, 5))
@@ -20,9 +23,18 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    lst_1 = list(str(ticket_number))
+    lst_2 = []
+    lst_2.append(lst_1[3:6])
+    del lst_1[3:6]
+    lst_1 = [int(i) for i in lst_1]
+    lst_2 = [int(el) for el in lst_2]
+    if lst_1[0] + lst_1[1] + lst_1[2] == lst_2[0] + lst_2[1] + lst_2[2]:
+        print('У вас счастливый билет!')
+    else:
+        print('У вас обычный билет')
+        # Не могу понять в чем ошибка, логика программы вроде бы правильная :(
 
 
 print(lucky_ticket(123006))
-print(lucky_ticket(12321))
 print(lucky_ticket(436751))
